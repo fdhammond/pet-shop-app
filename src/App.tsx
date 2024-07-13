69ec114727a7cb45a7b1d292207097ff8273f299
fdhammond <federico.hammond8@gmail.com>
2024-07-12 22:58:02 -0300

    delete unused code in app file

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,8 @@
-import React, { useEffect, useState } from 'react';
-import { fetchProducts } from './api/api';
+import React from 'react';
 import Table from './components/Table';
 import './App.css';
 
 const App: React.FC = () => {
-  // const [products, setProducts] = useState<Product[]>([]);
-  // const [loading, setLoading] = useState<boolean>(true);
-  // const [error, setError] = useState<string | null>(null);
-
-  // useEffect(() => {
-  //   const getProducts = async () => {
-  //     try {
-  //       const data: Product[] = await fetchProducts();
-  //       console.log(data);
-
-  //       setProducts(data);
-  //     } catch (err) {
-  //       setError((err as Error).message);
-  //     } finally {
-  //       setLoading(false);
-  //     }
-  //   };
-
-  //   getProducts();
-  // }, []);
-
-  // if (loading) return <p>Loading...</p>;
-  // if (error) return <p>Error: {error}</p>;
-
   return (
     <div className="App">
       <Table />
